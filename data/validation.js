3578fe80ed5fde67a8b9928e57716c0656ed4523
Anchal Singh <113389989+anzsiz@users.noreply.github.com>
2023-05-08 22:17:36 -0400

    Update validation.js

--- data/validation.js
+++ data/validation.js
@@ -1,79 +1,54 @@
 import emailValidator from 'email-validator';
 import { ObjectId } from 'mongodb';
 
-function validString(string) {
-  if (typeof string !== 'string' || !string) {
-    throw new Error('Invalid string');
-  }
-  if (string.trim().length < 1) {
-    throw new Error('Invalid string');
-  }
+export function validString(string) {
+  if (typeof string !== 'string' || !string) return false;
+  if (string.trim().length < 1) return false;
   return true;
 }
 
-function validEmail(email) {
-  if (!emailValidator.validate(email)) {
-    throw new Error('Invalid email');
-  }
-  return true;
+export function validEmail(email) {
+  return emailValidator.validate(email);
 }
 
-function validDate(date) {
+export function validDate(date) {
   validString(date);
   let splitDate = date.split('/');
 
-  if (splitDate[0].length > 2 || splitDate[0].length < 1) {
-    throw new Error('Invalid date');
-  }
-  if (splitDate[1].length > 2 || splitDate[0].length < 1) {
-    throw new Error('Invalid date');
-  }
-  if (splitDate[2].length !== 4) {
-    throw new Error('Invalid date');
-  }
+  if (splitDate[0].length > 2 || splitDate[0].length < 1) return false;
+  if (splitDate[1].length > 2 || splitDate[0].length < 1) return false;
+  if (splitDate[2].length !== 4) return false;
 
   let dateObj = new Date(date);
-  if (!dateObj) {
-    throw new Error('Invalid date');
-  }
+  if (!dateObj) return false;
 
   return true;
 }
 
-function validAge(date) {
+export function validAge(date) {
   let today = new Date();
   let dob = new Date(date);
   let age = today.getFullYear() - dob.getFullYear();
   if (
     today.getMonth() < dob.getMonth() ||
-    (today.getMonth() === dob.getMonth() && today.getDate < dob.getDate())
+    (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate())
   ) {
     age -= 1;
   }
 
-  if (age < 21) {
-    throw new Error('Invalid age');
-  }
+  if (age < 21) return false;
   return true;
 }
 
-function validId(id) {
-  if (!ObjectId.isValid(id) || !validString(id)) {
-    throw new Error('Invalid ID');
-  }
+export function validId(id) {
+  if (!ObjectId.isValid(id) || !validString(id)) return false;
   return true;
 }
 
-function validPassword(password) {
-  if (!validString(password)) {
-    throw new Error('Invalid password');
-  }
-  if (password.length < 8) {
-    throw new Error('Invalid password');
-  }
-  if (password === password.toLowerCase()) {
-    throw new Error('Invalid password');
-  }
+export function validPassword(password) {
+  if (!validString(password)) return false;
+  if (password.length < 8) return false;
+  if (password === password.toLowerCase()) return false;
 
   let pwSplit = password.split('');
   let count = 0;
@@ -82,9 +57,7 @@ function validPassword(password) {
     if (typeof numCheck === 'number') count += 1;
   }
 
-  if (count < 1) {
-    throw new Error('Invalid password');
-  }
+  if (count < 1) return false;
   return true;
 }
 
