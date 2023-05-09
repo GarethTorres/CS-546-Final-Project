0965b490f7dd366d55349e81431a0a2e82d4b744
Haitian3 <103725941+runatonce@users.noreply.github.com>
2023-05-09 05:35:40 -0400

    update
    update

--- app.js
+++ app.js
@@ -1,47 +1,3 @@
-<<<<<<< Updated upstream
-// Import required modules
-import express from 'express';
-import exphbs from 'express-handlebars';
-
-// Create an instance of the express application
-const app = express();
-
-// Configure Handlebars as the view engine
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-// Define routes
-app.get('/', (req, res) => {
-  // Render the homepage.handlebars template
-  res.render('homepage');
-});
-
-app.get('/createshowcase', (req, res) => {
-  // Render the createshowcase.handlebars template
-  res.render('createshowcase');
-});
-
-app.get('/login', (req, res) => {
-  // Render the login.handlebars template
-  res.render('login');
-});
-
-app.get('/searchresults', (req, res) => {
-  // Render the searchresults.handlebars template
-  res.render('searchresults');
-});
-
-app.get('/showcase', (req, res) => {
-  // Render the showcase.handlebars template
-  res.render('showcase');
-});
-
-// Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
-=======
 import express, { static as _static, json, urlencoded } from "express";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -88,4 +44,3 @@ app.listen(3000, () => {
     console.log("We've now got a server!");
     console.log("Your routes will be running on http://localhost:3000");
 });
->>>>>>> Stashed changes
--- data/index.js
+++ data/index.js
@@ -1,16 +1,3 @@
-<<<<<<< Updated upstream
-
-import userData from "./users";
-import showcaseData from './showcases';
-import blogData from './blogs';
-import commentData from "./comments";
-export default {
-  users: userData,
-  showcases: showcaseData,
-  blogs: blogData,
-  comments: commentData
-};
-=======
 import * as userData from './users.js';
 import * as showcaseData from './showcases.js';
 import * as commentData from './comments.js';
@@ -21,4 +8,4 @@ export const showcases = showcaseData;
 export const users = userData;
 export const reports = reportData;
 
->>>>>>> Stashed changes
+
