5417ffbc346003479e28c7812005e25610757866
Anchal Singh <113389989+anzsiz@users.noreply.github.com>
2023-05-08 20:10:09 -0400

    Update login.js

--- data/login.js
+++ data/login.js
@@ -1,23 +1,16 @@
-import { ObjectId } from 'mongodb';
-import { login } from '../config/mongoCollections';
-
-const validateLogin = async (username, password) => {
-    try {
-        if (typeof username !== 'string') {
-            throw new Error('Username must be a string!');
-        }
-        if (username.trim().length === 0) {
-            throw new Error('Username must not be empty!');
-        }
-        if (typeof password !== 'string') {
-            throw new Error('Password must be a string!');
-        }
-        if (password.trim().length === 0) {
-            throw new Error('Password must not be empty!');
-        }
-    } catch (error) {
-        throw error;
-    }
+const validateLogin = (userid, password) => {
+  if (typeof userid !== 'string') {
+    throw new Error('Error: User ID must be a string!');
+  }
+  if (userid.trim().length === 0) {
+    throw new Error('Error: User ID must not be empty!');
+  }
+  if (typeof password !== 'string') {
+    throw new Error('Error: Password must be a string!');
+  }
+  if (password.trim().length === 0) {
+    throw new Error('Error: Password must not be empty!');
+  }
 };
 
 export default { validateLogin };
