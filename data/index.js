06fe304efad86303d2bd608cfdacdb9be5f7761a
Haitian3 <103725941+runatonce@users.noreply.github.com>
2023-05-09 05:30:42 -0400

    update
    update

--- data/index.js
+++ data/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 import userData from "./users";
 import showcaseData from './showcases';
@@ -9,3 +10,15 @@ export default {
   blogs: blogData,
   comments: commentData
 };
+=======
+import * as userData from './users.js';
+import * as showcaseData from './showcases.js';
+import * as commentData from './comments.js';
+import * as reportData from './reports.js';
+
+export const comments = commentData;
+export const showcases = showcaseData;
+export const users = userData;
+export const reports = reportData;
+
+>>>>>>> Stashed changes
